b33ef816609b309489ce04bf9c2741ee9131e202
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-12 01:35:47 +0530

    Add multiplication table output example

---
+++ Basic_programs/Multiplication Table Using for Loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
+
+//Output:
+Enter a number: 5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
