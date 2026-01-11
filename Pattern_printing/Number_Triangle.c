3b75449ec36db19beebe7828256c64dc874f9df1
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-11 15:20:06 +0530

    Add Number Triangle pattern printing program
    This program prints a number triangle pattern up to 5 rows.

---
+++ Pattern_printing/Number_Triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//Output:
+1
+12
+123
+1234
+12345
+
