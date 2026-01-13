5419a15469389faa8132450194cd5819e2c1dd76
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-13 14:51:08 +0530

    Add relational operator program in C
    This program takes two integers as input and prints the results of various relational operations between them.

---
+++ Basic_programs/Relational operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    scanf("%d %d", &x,&y);
+    printf("%d\n",x>y);
+    printf("%d\n",x<y);
+    printf("%d\n",x>=y);
+    printf("%d\n",x<=y);
+    printf("%d\n",x==y);
+    printf("%d\n",x!=y);
+    
+    return 0;
+}
+
+//output:
+12
+23
+0
+1
+0
+1
+0
+1
