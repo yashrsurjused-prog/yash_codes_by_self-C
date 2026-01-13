9a3d90ceab474257095cca4f954922b79e7130e9
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-13 14:41:28 +0530

    Add arithmetic operators program in C
    Implement basic arithmetic operations in C.

---
+++ Basic_programs/Arithmetic operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+   int x,y;
+   scanf("%d\n", &x);
+   scanf("%d", &y);
+   printf("Addition: %d\n",x+y);
+   printf("Asubstraction: %d\n",x-y);
+   printf("dIVSION: %d\n",x/y);
+   printf("multiplication: %d\n",x*y);
+   printf("division: %d\n",x%y);
+    return 0;
+}
+
+//Output:
+65
+78
+Addition: 143
+Asubstraction: -13
+dIVSION: 0
+multiplication: 5070
+division: 65
