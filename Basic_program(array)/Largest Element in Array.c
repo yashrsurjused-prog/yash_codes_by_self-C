e0d8e889c424f525dee074cdcd4972bdd196b764
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-14 16:46:17 +0530

    Create Largest Element in Array.c

---
+++ Basic_program(array)/Largest Element in Array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a[100];
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] > max)
+            max = a[i];
+    }
+
+    printf("Largest = %d", max);
+
+    return 0;
+}
+
+//output:
+//input:
+5
+3 9 2 15 7
+//output:
+Largest = 15
