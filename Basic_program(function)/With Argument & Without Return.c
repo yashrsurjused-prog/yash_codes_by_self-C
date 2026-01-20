72494355423958a619bb3b7da9ec2906fda7aaff
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-20 12:27:18 +0530

    Create With Argument & Without Return.c

---
+++ Basic_program(function)/With Argument & Without Return.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// function definition (with argument, no return)
+void show(int a) {
+    // printing value received
+    printf("Number = %d", a);
+}
+
+int main() {
+    // function call with argument
+    show(10);
+    return 0;
+}
+
+//output:
+Number = 10
