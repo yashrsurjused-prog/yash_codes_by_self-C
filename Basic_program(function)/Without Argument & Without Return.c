d4c43536c570f3e698f1985572e6d923355dcb50
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-20 12:26:20 +0530

    Create Without Argument & Without Return.c

---
+++ Basic_program(function)/Without Argument & Without Return.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// function definition (no argument, no return)
+void show() {
+    // printing message
+    printf("Hello C");
+}
+
+int main() {
+    // function call
+    show();
+    return 0;
+}
+
+//output:
+Hello C
